5ee7d642bc4706ddea93363eaa6d5be189629895
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-20 10:19:52 -0400

    creates rover bot hello world

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rover Bot Hello World
--- server/server.js
+++ server/server.js
@@ -13,11 +13,13 @@ const nano = new SerialPort({
   baudRate: 9600,
 });
 
-app.get('/bot-move/', (req, res) => {
-    console.log(req.body);
-    nano.write(`<${dir},${spd}>`);
+app.get('/bot-test/:value', (req, res) => {
+    console.log(req.params);
+    nano.write(`${req.params.value}`);
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 });
+
